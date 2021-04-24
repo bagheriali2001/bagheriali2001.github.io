068b19d909f30f93758de1aae818c06055756ea2
bagheriali2001 <alibagheri.1379.1389@gmail.com>
2021-04-24 23:35:22 +0430

    bug fixed

--- script/app.js
+++ script/app.js
@@ -8,16 +8,16 @@ const text = [`<h5>Check my resume</h5>`,
         <li><a href="https:/twitter.com/bagheriali2001">Twitter</a></li>
         <li><a href="https://telegram.me/bagheriali2001">Telegram</a></li>
     </ul>
-    <p>C:\Users\Ali>TYPE resume.txt</p>`,
+    <p>C:\\Users\\Ali>TYPE resume.txt</p>`,
     `<h3>Contact information and links</h3>
-    <p>C:\Users\Ali>TYPE links-list.txt</p>`,
+    <p>C:\\Users\\Ali>TYPE links-list.txt</p>`,
     `<ul>
         <li><a href="https://github.com/bagheriali2001/Web-midterm-project">Noshirvani institute of technology, information technology center website (as Back-end developer)</a></li>
         <li><a href="https://github.com/bagheriali2001/Ecommerce-REST-API">Ecommerce-REST-API</a></li>
     </ul>
-    <p>C:\Users\Ali>ECHO Contact information and links</p>`,
+    <p>C:\\Users\\Ali>ECHO Contact information and links</p>`,
     `<h3>Projects</h3>
-    <p>C:\Users\Ali>TYPE projects-list.txt</p>`,
+    <p>C:\\Users\\Ali>TYPE projects-list.txt</p>`,
     `<ul>
         <li>Node.js</li>
         <li>Express.js</li>
@@ -31,9 +31,9 @@ const text = [`<h5>Check my resume</h5>`,
         <li>C++</li>
         <li>Java</li>
     </ul>
-    <p>C:\Users\Ali>ECHO Projects</p>`,
+    <p>C:\\Users\\Ali>ECHO Projects</p>`,
     `<h3>Profissional skills</h3>
-    <p>C:\Users\Ali>TYPE skills-list.txt</p>`,
+    <p>C:\\Users\\Ali>TYPE skills-list.txt</p>`,
     `<p>I am a hardworking man, whose challenges could never stop.
         I am a self-learner who wouldn't take a rest when it is hard to learn.
         I am a good team player who would help others in the team to reach our goals.
@@ -42,18 +42,18 @@ const text = [`<h5>Check my resume</h5>`,
         I try to be like a diamond when I am under pressure. I would adapt to hard situations to have my best performance.
         I am always interested to try new things and gain experience.
     </p>
-    <p>C:\Users\Ali>ECHO Profissional skills</p>`,
+    <p>C:\\Users\\Ali>ECHO Profissional skills</p>`,
     `<h3>About me</h3>
-    <p>C:\Users\Ali>TYPE about-me.txt</p>`,
+    <p>C:\\Users\\Ali>TYPE about-me.txt</p>`,
     `<p>I am a computer engineer student at Babol Noshirvani institute of technology,
         who is interested in AI and Back-end development. I am currently working as a junior back-end developer.
         I am living in <a href="https://www.google.com/maps/place/Arak,+Markazi+Province,+Iran">Arak</a> but I am open to relocating for positions in <a href="https://www.google.com/maps/place/Tehran,+Tehran+Province,+Iran">Tehran</a>.</p>
-    <p>C:\Users\Ali>ECHO About me</p>`,
+    <p>C:\\Users\\Ali>ECHO About me</p>`,
     `<h3>Who am I?</h3>
-    <p>C:\Users\Ali>TYPE who-am-i.txt</p>`,
+    <p>C:\\Users\\Ali>TYPE who-am-i.txt</p>`,
     `<h1>Ali Bagheri</h1>
-    <p>C:\Users\Ali>ECHO Who am I?</p>`,
-    `<p>C:\Users\Ali>TYPE name.txt</p>`
+    <p>C:\\Users\\Ali>ECHO Who am I?</p>`,
+    `<p>C:\\Users\\Ali>TYPE name.txt</p>`
 ]
 
 window.addEventListener("keyup",(e)=>{ 
